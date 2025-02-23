d77a62ecdaba56b263527efe9d2e0ae9d9b0b248
svrkrishna-ch <ramakrishna.chilla074@gmail.com>
2025-02-23 08:41:57 +0530

    conversation added

---
+++ 04-variables.sh
@@ -0,0 +1,10 @@
+#!bin/bash/
+
+PERSON1=$1
+PERSON2=$2
+PERSON3=$3
+
+echo "$PERSON1: Hi $PERSON2"
+echo "$PERSON2: Hello $PERSON1"
+echo "$PERSON1: How is my sister ${PERSON3}?"
+echo "$PERSON3: I'm good brother"

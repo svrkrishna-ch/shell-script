5aa67eab8df498a6b3840747b0a74b90a8642f63
svrkrishna-ch <ramakrishna.chilla074@gmail.com>
2025-02-23 08:37:46 +0530

    conversation added

--- 03-variables.sh
+++ 03-variables.sh
@@ -6,5 +6,5 @@ PERSON3=Parvathi
 
 echo "$PERSON1: Hi $PERSON2"
 echo "$PERSON2: Hello $PERSON1"
-echo "$PERSON1: How is my sister $PERSON3?"
+echo "$PERSON1: How is my sister ${PERSON3}?"
 echo "$PERSON3: I'm good brother"

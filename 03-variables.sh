b46a20ae6604893d237221b92823800dd2fd7a90
svrkrishna-ch <ramakrishna.chilla074@gmail.com>
2025-02-23 08:32:14 +0530

    conversation added

---
+++ 03-variables.sh
@@ -0,0 +1,10 @@
+#!bin/bash/
+
+PERSON1=Vishnu
+PERSON2=Shiva
+PERSON3=Parvathi
+
+echo "$PERSON1: Hi $PERSON2"
+echo "$PERSON2: Hello $PERSON1"
+echo "$PERSON1: How is my sister $PERSON3?"
+echo "$PERSON3: I'm good brother"

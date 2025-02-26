4435d95932e48c52ec28e826cadceb8e6320629f
svrkrishna-ch <ramakrishna.chilla074@gmail.com>
2025-02-26 06:35:18 +0530

    shell script

--- 10-install-script.sh
+++ 10-install-script.sh
@@ -10,4 +10,20 @@ fi
 
 dnf install mysqll -y
 
+if [ $? -ne 0 ]
+then 
+    echo "Installing Mysql - FIALURE"
+    exit 1
+else
+    echo "Installing Mysql - SUCCESS"
+fi
+
 dnf install git -y
+
+if [ $? -ne 0 ]
+then 
+    echo "Installing Git - FIALURE"
+    exit 1
+else
+    echo "Installing Git - SUCCESS"
+fi

cf32809cbea61fe876c2f7e2a18acdf82f0c30f4
svrkrishna-ch <ramakrishna.chilla074@gmail.com>
2025-02-26 06:25:19 +0530

    shell script

--- 10-install-script.sh
+++ 10-install-script.sh
@@ -5,25 +5,6 @@ USERID=$(id -u)
 if [ $USERID -ne 0 ]
 then 
     echo "ERROR:: You must have sudo access to execute this script"
-    exit 1
 fi
-
+ 
 dnf install mysqll -y
-
-if [ $? -ne 0 ]
-then 
-    echo "Installing Mysql - FIALURE"
-    exit 1
-else
-    echo "Installing Mysql - SUCCESS"
-fi
-
-dnf install git -y
-
-if [ $? -ne 0 ]
-then 
-    echo "Installing Git - FIALURE"
-    exit 1
-else
-    echo "Installing Git - SUCCESS"
-fi

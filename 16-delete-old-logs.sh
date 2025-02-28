28c4d8bdf2626e1a3dcf7149a066481bbeaa4e61
svrkrishna-ch <ramakrishna.chilla074@gmail.com>
2025-02-28 08:51:56 +0530

    shell script

--- 16-delete-old-logs.sh
+++ 16-delete-old-logs.sh
@@ -6,7 +6,7 @@ G="\e[32m"
 Y="\e[33m"
 N="\e[0m"
 
-SOURCE_DIR="/var/log/shellscript-logs"
+SOURCE_DIR="/home/ec2-user/app-logs"
 LOG_FILE=$(echo $0 | cut -d "." -f1 )
 TIMESTAMP=$(date +%y-%m-%d-%H-%M-%S)
 LOG_FILE_NAME="$LOGS_FOLDER/$LOG_FILE-$TIMESTAMP.log"

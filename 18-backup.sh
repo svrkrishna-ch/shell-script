a2438af2bc3e0c3ebfbf466e40e406da6614933a
svrkrishna-ch <ramakrishna.chilla074@gmail.com>
2025-03-02 05:44:01 +0530

    shell script

---
+++ 18-backup.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+USERID=$(id -u)
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+SOURCE_DIR=$1
+DEST_DIR=$2
+DAYS=${3:-14} # if user is not providing no.of days, we are taking 14 as default
+
+LOGS_FOLDER="/var/log/shellscript-logs"
+LOG_FILE=$(echo $0 | cut -d "." -f1 )
+TIMESTAMP=$(date +%y-%m-%d-%H-%M-%S)
+LOG_FILE_NAME="$LOGS_FOLDER/$LOG_FILE-$TIMESTAMP.log"
+
+VALIDATE(){
+    if [ $1 -ne 0 ]
+    then 
+        echo -e "$2 ... $R FAILURE $N"
+        exit 1
+    else
+        echo -e "$2 ... $G SUCCESS $N"
+    fi
+}
+
+USAGE(){
+    echo -e "$R USAGE:: $N sh 18-backupsh <SOURCE_DIR> <DEST_DIR> <DAYS(Optional)>" 
+}
+
+if [$# -lt 2 ]
+then
+    USAGE
+fi
+
+
+echo "Script started executing at $TIMESTAMP" &>>$LOG_FILE_NAME

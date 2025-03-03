2e75a18219750c537217ec8b7ac92668a066e74e
svrkrishna-ch <ramakrishna.chilla074@gmail.com>
2025-03-03 05:43:45 +0530

    shell script

--- 18-backup.sh
+++ 18-backup.sh
@@ -35,4 +35,14 @@ mkdir -p /home/ec2-user/shellscript-logs/
 if [ $# -lt 2 ]
 then
     USAGE
-fi 
+fi
+
+if [ ! -d <SOURCE_DIR> ]
+then
+    echo -e "Source Directory does not exist...Please check"
+fi
+
+if [ ! -d <DEST_DIR> ]
+then
+    echo -e "Destination Directory does not exist...Please check"
+fi
